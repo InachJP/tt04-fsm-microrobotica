d7bd47492b11c1738f014c873ebf186277f5f3b5
jpedraza <jonathan.pedraza@usm.cl>
2023-08-21 15:43:17 -0400

    actualizaciones

--- src/test.py
+++ src/test.py
@@ -2,47 +2,65 @@ import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
 
-
-segments = [ 63, 6, 91, 79, 102, 109, 124, 7, 127, 103 ]
-
 @cocotb.test()
-async def test_7seg(dut):
-    dut._log.info("start")
-    clock = Clock(dut.clk, 10, units="us")
-    cocotb.start_soon(clock.start())
+async def test_controlador_microbots(dut):
+    # Initialize the clock
+    clock = Clock(dut.clk, 10, units="ns")
+    cocotb.fork(clock.start())
 
-    # reset
-    dut._log.info("reset")
-    dut.rst_n.value = 0
-    # set the compare value
-    dut.ui_in.value = 1
-    await ClockCycles(dut.clk, 10)
-    dut.rst_n.value = 1
+    # Reset the module
+    dut.rst_n <= 0
+    await RisingEdge(dut.clk)
+    dut.rst_n <= 1
+    await RisingEdge(dut.clk)
 
-    # the compare value is shifted 10 bits inside the design to allow slower counting
-    max_count = dut.ui_in.value << 10
-    dut._log.info(f"check all segments with MAX_COUNT set to {max_count}")
-    # check all segments and roll over
-    for i in range(15):
-        dut._log.info("check segment {}".format(i))
-        await ClockCycles(dut.clk, max_count)
-        assert int(dut.segments.value) == segments[i % 10]
+    # Define states and transitions for the test
+    states = {
+        "Standby": 0,
+        "goforward": 1,
+        "goright": 2,
+        "goleft": 3
+    }
 
-        # all bidirectionals are set to output
-        assert dut.uio_oe == 0xFF
+    transitions = [
+        ("Standby", "goforward"),
+        ("goforward", "goforward"),
+        ("goforward", "Standby"),
+        ("Standby", "goright"),
+        ("goright", "goright"),
+        ("goright", "Standby"),
+        ("Standby", "goleft"),
+        ("goleft", "goleft"),
+        ("goleft", "Standby")
+    ]
 
-    # reset
-    dut.rst_n.value = 0
-    # set a different compare value
-    dut.ui_in.value = 3
-    await ClockCycles(dut.clk, 10)
-    dut.rst_n.value = 1
+    # Run the test
+    state = "Standby"
+    for transition in transitions:
+        from_state, to_state = transition
+        # Set input sensors based on current state
+        if from_state == "Standby":
+            dut.f_sensor <= 0
+            dut.l_sensor <= 0
+            dut.r_sensor <= 0
+        elif from_state == "goforward":
+            dut.f_sensor <= 0
+            dut.l_sensor <= 1
+            dut.r_sensor <= 1
+        elif from_state == "goright":
+            dut.f_sensor <= 1
+            dut.l_sensor <= 1
+            dut.r_sensor <= 0
+        elif from_state == "goleft":
+            dut.f_sensor <= 1
+            dut.l_sensor <= 0
+            dut.r_sensor <= 1
+        
+        await ClockCycles(dut.clk, 1)
+        
+        assert states[state] == int(dut.state)
 
-    max_count = dut.ui_in.value << 10
-    dut._log.info(f"check all segments with MAX_COUNT set to {max_count}")
-    # check all segments and roll over
-    for i in range(15):
-        dut._log.info("check segment {}".format(i))
-        await ClockCycles(dut.clk, max_count)
-        assert int(dut.segments.value) == segments[i % 10]
+        state = to_state
+        await Timer(1, units="ns")
 
+    cocotb.log.info("Test completed successfully")
---
+++ src/tt_um_controlador_microbots.v
@@ -0,0 +1,118 @@
+`timescale 1ns / 1ps
+
+/*
+Universidad técnica Federico Santa María, Valparaíso
+Autor: Jonathan Pedraza & Lucas Irribarra
+*/
+
+module tt_um_controlador_microbots#(
+    input  wire [2:0] ui_in,    // Dedicated inputs
+    output wire [3:0] uo_out,   // Dedicated outputs
+    input  wire [7:0] uio_in,   // IOs: Input path
+    output wire [7:0] uio_out,  // IOs: Output path
+    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // will go high when the design is enabled
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
+);
+    wire reset, f_sensor, l_sensor, r_sensor, motorA_d, motorA_i, motorB_d, motorB_i;
+    wire [3:0] motors;
+
+    assign uio_oe = 8'b1111_1111; //todos output
+    assign {f_sensor, l_sensor, r_sensor} = ui_in;
+    assign uo_out = motors;
+    assign motors[0] = motorB_i;
+    assign motors[1] = motorB_d;
+    assign motors[2] = motorA_i;
+    assign motors[3] = motorB_d;
+    assign reset = ~rst_n;
+
+ reg [1:0] state, next_state;
+
+parameter Standby = 2'b00;
+parameter goforward = 2'b01;
+parameter goright = 2'b10;
+parameter goleft = 2'b11;
+
+always @(posedge clk) begin
+    if (reset)
+        state <= Standby;
+    else
+        state <= next_state;
+end
+
+always @* begin
+    next_state = Standby;
+    case (state)
+        Standby: 
+            if (f_sensor == 0 && l_sensor == 0 && r_sensor == 0)
+            begin
+                next_state = goforward;
+            end
+            else if (f_sensor == 0 && l_sensor == 1 && r_sensor == 1)
+            begin
+                next_state = goforward;
+            end
+            else if (l_sensor == 1 && r_sensor == 0)
+            begin
+                next_state = goright;
+            end
+            else if (f_sensor == 1 && r_sensor == 0)
+            begin
+                next_state = goright;
+            end
+            else if (l_sensor == 0 && r_sensor == 1)
+            begin
+                next_state = goleft;
+            end
+        goforward: 
+            if (f_sensor == 0 && l_sensor == 0 && r_sensor == 0)
+            begin
+                next_state = state;
+            end
+            if (f_sensor == 0 && l_sensor == 1 && r_sensor == 1)
+            begin
+                next_state = state;
+            end
+        goright: 
+            if (l_sensor == 1 && r_sensor == 0)
+            begin
+                next_state = state;
+            end
+        goleft: 
+            if (l_sensor == 0 && r_sensor == 1)
+            begin
+                next_state = state;
+            end
+    endcase
+end
+
+always @* begin //se definen las polarizaciones de los motores 
+    case (state)
+        Standby : begin
+            motorB_i = 0;
+            motorB_d = 0;
+            motorA_i = 0;
+            motorA_d = 0;
+        end
+        goforward: begin
+            motorB_i = 0;
+            motorB_d = 1;
+            motorA_i = 0;
+            motorA_d = 1;
+        end
+        goright: begin
+            motorB_i = 1;
+            motorB_d = 0;
+            motorA_i = 0;
+            motorA_d = 1;
+        end
+        goleft: begin
+            motorB_i = 0;
+            motorB_d = 1;
+            motorA_i = 1;
+            motorA_d = 0;
+        end
+    endcase
+end
+endmodule
