9d28ca1cfe973dbb082aa18da5999a920b947194
jpedraza <jonathan.pedraza@usm.cl>
2023-08-21 16:07:12 -0400

    se agrega espacio entre #

--- src/tt_um_controlador_microbots.v
+++ src/tt_um_controlador_microbots.v
@@ -5,7 +5,7 @@ Universidad técnica Federico Santa María, Valparaíso
 Autor: Jonathan Pedraza & Lucas Irribarra
 */
 
-module tt_um_controlador_microbots#(
+module tt_um_controlador_microbots #(
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
