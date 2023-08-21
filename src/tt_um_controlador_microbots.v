d605db45db08616743995765d996cf418cc8695e
jpedraza <jonathan.pedraza@usm.cl>
2023-08-21 15:53:07 -0400

    se deja formato de entradas como estaba antes

--- src/tt_um_controlador_microbots.v
+++ src/tt_um_controlador_microbots.v
@@ -6,8 +6,8 @@ Autor: Jonathan Pedraza & Lucas Irribarra
 */
 
 module tt_um_controlador_microbots#(
-    input  wire [2:0] ui_in,    // Dedicated inputs
-    output wire [3:0] uo_out,   // Dedicated outputs
+    input  wire [7:0] ui_in,    // Dedicated inputs
+    output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
     output wire [7:0] uio_out,  // IOs: Output path
     output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
